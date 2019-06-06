ca9d612864c0eb944718765da2c64653c65bdf72
vdprog2019 <48090405+vdprog2019@users.noreply.github.com>
2019-06-06 14:43:15 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,21 +3,30 @@
 $l = $_POST["login_php"];
 $p = $_POST["password_php"];
 
+
+/*
 echo "<table>
     <tr><td>Логин</td><td>$l</td></tr> 
     <tr><td>Пароль</td><td>$p</td></tr> 
 </table>
 ";
+*/
 
 
-
-if (filter_var($l, FILTER_VALIDATE_URL) === true) {
+if (filter_var($l, FILTER_VALIDATE_URL,FILTER_VALIDATE_FLOAT) === true) {
     die('Not a valid');
+}else{
+    echo "<table>
+    <tr><td>Логин</td><td>$l</td></tr> 
+</table>
+";
 }
 
-if (filter_var($p, FILTER_VALIDATE_URL) === true) {
-    die('Not a valid');
-}
+
+    echo "<table>
+    <tr><td>Пароль</td><td>$p</td></tr> 
+</table>
+";
 
 
 
