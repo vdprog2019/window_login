b64fa3c1d593599479bec6292dce9900fd9c5f14
vdprog2019 <48090405+vdprog2019@users.noreply.github.com>
2019-06-06 14:54:20 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-$l = $_POST["login_php"];
-$p = $_POST["password_php"];
-
-
 /*
 echo "<table>
     <tr><td>Логин</td><td>$l</td></tr> 
@@ -12,21 +8,29 @@ echo "<table>
 ";
 */
 
+//!empty( имя_переменной)   -   проверяет что поле не пустое
+//if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+if(!empty($_POST["login_php"]) && !empty($_POST["password_php"])){
+    $l = $_POST["login_php"];
+    $p = $_POST["password_php"];
 
-if (filter_var($l, FILTER_VALIDATE_URL,FILTER_VALIDATE_FLOAT) === true) {
-    die('Not a valid');
-}else{
-    echo "<table>
+    if (filter_var($l, FILTER_VALIDATE_URL, FILTER_VALIDATE_FLOAT) === true) {
+        die('Not a valid');
+    } else {
+        echo "<table>
     <tr><td>Логин</td><td>$l</td></tr> 
 </table>
 ";
-}
+    }
 
 
     echo "<table>
     <tr><td>Пароль</td><td>$p</td></tr> 
 </table>
 ";
+}
+else die("NOT FOUND!");
+
 
 
 
